d53eb460dbfa3457f77caf9735bd3ced2418bccd
feehaam <mdfeeham@gmail.com>
2024-09-01 15:25:43 +0600

    Server first added for db creation.

--- src/main/java/playground/data/server_first_approach/PartitionInitSub.java
+++
@@ -1,8 +0,0 @@
-package playground.data.server_first_approach;
-
-public class PartitionInitSub extends PartitionInit{
-    @Override
-    public String getQ(){
-        return "CREATE TABLE IF NOT EXISTS tablexx (id INT PRIMARY KEY, name VARCHAR(255))";
-    }
-}
--- src/main/java/playground/data/server_first_approach/import.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS example_partitionxxx (id INT, name VARCHAR(255))
-INSERT INTO ITEM (name) VALUES ('Books');
-INSERT INTO ITEM (name) VALUES ('Games');
